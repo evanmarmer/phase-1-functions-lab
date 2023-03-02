60dfadc9e477cb3f1530f69a54a7fe751dda8b88
evanmarmer <evanmarmer@gmail.com>
2023-03-02 13:23:29 -0700

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution in this file!
+
+ const Hq = 42
+ function distanceFromHqInBlocks(location){
+     if (location < Hq){
+         return Hq - location
+     }
+     return location - Hq;
+ }
+
+function distanceFromHqInFeet(location){
+    return distanceFromHqInBlocks(location)*264
+}
+
+
+
+
+
+
+
+function distanceTravelledInFeet(start, destination){
+    if (start > destination){
+        return (start - destination)*264
+    }
+    return (destination - start)*264
+}
+
+function calculatesFarePrice(start, destination){
+
+  let tripDistance =  distanceTravelledInFeet(start, destination)
+
+   if (tripDistance <400){
+    return 0
+   }
+   
+   if (tripDistance >400 && tripDistance <2000){
+    return (tripDistance -400) *.02
+   }
+
+   if (tripDistance >2000 && tripDistance <2500){
+    return 25
+   } 
+
+   if (tripDistance >2500){
+    return 'cannot travel that far'
+   }
+}
